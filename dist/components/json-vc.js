eb94dd5da985c78bf8596cf117097d8b4a1c9834
Diego F. Goberna <diego@feiss.be>
2016-08-17 02:16:50 +0200

    added json-vc component
    - json-vc: support for json models with vertex colors
    - added 3 models in json format

---
+++ dist/components/json-vc.js
@@ -0,0 +1,29 @@
+AFRAME.registerComponent('json-vc', {
+  schema: {
+    src: {type: 'src'},
+  },
+  init: function () {
+    var objectLoader;
+    var object3D = this.el.object3D;
+    if (this.objectLoader) { return; }
+    objectLoader = this.objectLoader = new THREE.ObjectLoader();
+    objectLoader.load(this.data.src, function(group) {
+      group.children.forEach(function(obj) {
+
+        //it is supposed that if you check "shadeless" in blender, threejs exporter sets the 
+        //material to MeshBasicMaterial, but it doesn't work for me, so I have to set it manually :/
+        //https://github.com/mrdoob/three.js/pull/7635
+
+        if (obj.name=='red' || obj.name=="sky" ){
+          obj.material= new THREE.MeshBasicMaterial( { vertexColors: true } );
+        }
+        else if (obj.name=='platform' ){
+          obj.material= new THREE.MeshLambertMaterial( { vertexColors: true } );;
+        }
+
+      });
+
+      object3D.add(group);
+    });
+  }
+});
