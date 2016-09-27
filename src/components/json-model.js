04732418f7f5174ba5b6f4f88021647dc000cb1e
Diego F. Goberna <diego@feiss.be>
2016-09-27 08:54:05 +0200

    placeholder set models

--- src/components/json-model.js
+++ src/components/json-model.js
@@ -10,22 +10,38 @@ AFRAME.registerComponent('json-model', {
     this.objectLoader.setCrossOrigin('');
   },
 
+  fixNormal: function (vector) {
+      var t = vector.y;
+      vector.y = -vector.z;
+      vector.z = t;
+  },
+
   update: function (oldData) {
     var self = this;
     var src = this.data.src;
     if (!src || src === oldData.src) { return; }
-    this.objectLoader.load(src, function (group) {
+
+    this.objectLoader.load(this.data.src, function (group) {
       var Rotation = new THREE.Matrix4().makeRotationX(-Math.PI / 2);
       group.traverse(function (child) {
         if (!(child instanceof THREE.Mesh)) { return; }
+ /*       child.position.applyMatrix4(Rotation);
+*/
+        child.geometry.faces.forEach( face => {
+          self.fixNormal(face.normal);
+          face.vertexNormals.forEach( vertex => {
+            if (!vertex.hasOwnProperty('fixed')) {
+              self.fixNormal(vertex);
+              vertex.fixed = true;
+            }
+          });
+        });
+        child.geometry.normalsNeedUpdate = true;
+        child.geometry.verticesNeedUpdate = true;
 
-        child.position.applyMatrix4(Rotation);
+        //child.material = new THREE.MeshPhongMaterial();
 
-        if (self.data.vertexcolors || child.name=='red' || child.name=="sky" || child.name=='glow') { // <= @hack this should be here
-          child.material= new THREE.MeshBasicMaterial({ vertexColors: true }); // <= neither this
-        }
       });
-
       self.el.setObject3D('mesh', group);
       self.el.emit('model-loaded', {format: 'json', model: group, src: src});
     });
--- src/index.js
+++ src/index.js
@@ -7,4 +7,5 @@ require('./components/enemy.js');
 require('./components/enemybullet.js');
 require('./components/gun.js');
 require('./components/headset.js');
+require('./components/json-vc.js');
 require('./components/json-model.js');
