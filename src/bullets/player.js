2f025907ed7dbf5f3c96f30fbd6fae8c68397262
Diego F. Goberna <diego@feiss.be>
2016-10-26 00:54:24 +0200

    tweaking bullets appearance and speed

--- src/bullets/player.js
+++ src/bullets/player.js
@@ -7,12 +7,12 @@ ASHOOTER.registerBullet(
     components: {
       bullet: {
         name: 'default',
-        maxSpeed: 5,
-        initialSpeed: 1,
-        acceleration: 0.2
+        maxSpeed: 1,
+        initialSpeed: 0.1,
+        acceleration: 0.4
       },
       'collision-helper': {
-        debug: true,
+        debug: false,
         radius: 0.1
       }
     },
@@ -22,18 +22,18 @@ ASHOOTER.registerBullet(
   {
     init: function () {
       var el = this.el;
-      el.setAttribute('geometry', {primitive: 'octahedron', radius: 0.1, detail: 1});
-      el.setAttribute('material', {shader: 'flat', color: '#ff0'});
+      el.setAttribute('geometry', {primitive: 'octahedron', radius: 0.08, detail: 1});
+      el.setAttribute('material', {shader: 'flat', color: '#24CAFF'});
     },
     reset: function () {
       var el = this.el;
-      el.setAttribute('material', 'color', '#ff0');
+      el.setAttribute('material', 'color', '#24CAFF');
       el.setAttribute('scale', {x: 1, y: 1, z: 1});
     },
     tick: function (time, delta) {
     },
     onHit: function (type) {
-      this.el.setAttribute('material', 'color', '#AAA');
+      this.el.setAttribute('material', 'color', '#FFF');
     }
   }
 );
