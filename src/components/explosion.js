7bd99c1391c54d81a6d162a8ca0fd05ff6c61f34
Fernando Serrano <fernandojsg@gmail.com>
2016-11-16 01:42:32 +0100

    Fixes explosions loadtexture

--- src/components/explosion.js
+++ src/components/explosion.js
@@ -20,7 +20,7 @@ AFRAME.registerComponent('explosion', {
     var parts = [
       {textureIdx: 2, billboard: true,  color: 16777215, scale: 1.5, grow: 4, dispersion: 0, copies: 1, speed: 0 },
       {textureIdx: 0, billboard: true,  color: 16777215, scale: 0.4, grow: 2, dispersion: 2.5, copies: 3, speed: 1 },
-      {textureIdx: 3, billboard: false, color: this.data.color, scale: 1, grow: 6, dispersion: 0, copies: 1, speed: 0 }, 
+      {textureIdx: 3, billboard: false, color: this.data.color, scale: 1, grow: 6, dispersion: 0, copies: 1, speed: 0 },
       {textureIdx: 1, billboard: true,  color: 16577633, scale: 0.04, grow: 2, dispersion: 3, copies: 20, speed: 2},
       {textureIdx: 3, billboard: true,  color: this.data.color, scale: 0.2, grow: 2, dispersion: 2, copies: 10, speed: 1}
     ];
@@ -38,14 +38,15 @@ AFRAME.registerComponent('explosion', {
       });
 
       this.materials.push(material);
-      this.el.sceneEl.systems.material.loadTexture(textureSrcs[part.textureIdx], {src: textureSrcs[part.textureIdx]}, setMap.bind(this, i));
+      var src = document.querySelector(textureSrcs[part.textureIdx]).getAttribute('src');
+      this.el.sceneEl.systems.material.loadTexture(src, {src: src}, setMap.bind(this, i));
       
       function setMap (idx, texture) {
         this.materials[idx].alphaMap = texture;
         this.materials[idx].needsUpdate = true;
       }
 
-      var dispersionCenter =  part.dispersion / 2; 
+      var dispersionCenter =  part.dispersion / 2;
 
       for (var n = 0; n < part.copies; n++) {
         var mesh = new THREE.Mesh(planeGeometry, material);
@@ -54,8 +55,8 @@ AFRAME.registerComponent('explosion', {
         }
         if (part.dispersion > 0) {
           mesh.position.set(
-            Math.random() * part.dispersion - dispersionCenter, 
-            Math.random() * part.dispersion - dispersionCenter, 
+            Math.random() * part.dispersion - dispersionCenter,
+            Math.random() * part.dispersion - dispersionCenter,
             Math.random() * part.dispersion - dispersionCenter
           );
           mesh.speed = part.speed + Math.random() / part.dispersion;
